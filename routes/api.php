69761fab46d702b617de290589ce50c5755237d7
Abdelrahman Rafeek <152920007+ShadowOfDeath25@users.noreply.github.com>
2025-08-04 22:43:55 +0300

    added missing user endpoint

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\PlanController;
 use App\Http\Controllers\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/user', function () {
+    return request()->user();
+});
 Route::apiResource("plans", PlanController::class)->middleware('auth:sanctum');
 Route::apiResource("subscriptions", SubscriptionController::class)->middleware('auth:sanctum');
 
